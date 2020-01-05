c5cdd639a5801911a4a3637160737008068e7b06
Mohamed El Mahallawy <mmahalwy@gmail.com>
2020-01-04 21:30:20 -0800

    remove styles

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import { config } from '../config';
-import { globalStyles } from '../styles';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
