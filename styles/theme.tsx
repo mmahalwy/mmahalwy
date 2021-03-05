861e27546065a2086c7a17c4ca0b9c2008ef9661
Mohamed El Mahallawy <mmahalwy@gmail.com>
2021-03-04 22:47:06 -0800

    fixy

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,7 +1,9 @@
-import { extendTheme } from '@chakra-ui/react';
+import { ColorMode, extendTheme } from '@chakra-ui/react';
+
+const initialColorMode: ColorMode = 'dark';
 
 const config = {
-  initialColorMode: 'dark',
+  initialColorMode,
 
   fonts: {
     body:
